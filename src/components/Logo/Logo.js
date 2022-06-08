9fc565bf59ab428926799ff9b5055269b41761f8
Yulia Bonohova <ybonohova@gmail.com>
2022-06-08 19:25:05 +0200

    added jsx and css for Login

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import './Logo.css';
+import LogoImg from '../../images/icon-logo.svg';
+import { Link } from 'react-router-dom';
+
+function Logo() {
+  return (
+    <Link to="/">
+      <img className="logo hover-button" src={LogoImg} alt="Логотип" />
+    </Link>
+  )
+}
+
+export default Logo;
