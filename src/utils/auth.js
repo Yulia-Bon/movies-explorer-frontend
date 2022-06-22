1c0b0fab3f8cb20d185ff74c3b4938fbed11dfab
Yulia Bonohova <ybonohova@gmail.com>
2022-06-22 13:52:43 +0200

    sign up and sign in

---
+++ src/utils/auth.js
@@ -0,0 +1,42 @@
+const BASE_URL = 'https://movies-express.ybon.nomoreparties.sbs';
+//const BASE_URL ='https://api.movies.krasnovid.nomoredomains.work' ||  'http://localhost:3000';
+
+export function register({ password, email, name }) {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ password, email, name })
+    })
+    .then(checkResponseStatus)
+}
+
+export function authorize({ password, email }) {
+    return fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ password, email })
+    })
+    .then(checkResponseStatus)
+};
+
+export function checkToken(jwt) {
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`,
+        }
+    })
+    .then(checkResponseStatus)
+}
+
+function checkResponseStatus(res) {
+    if (res.ok) {
+        return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`)
+}
